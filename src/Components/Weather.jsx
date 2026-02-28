34a1bdf8a40c5dc7f432116412c9e93a22882151
flutternotasco <flutternotasco@gmail.com>
2026-02-28 16:12:30 +0530

    test1

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -89,7 +89,7 @@ search("London");
                 <img src={wind_icon} alt=''/>
                 <div>
                   <p>{weatherData.windSpeed} km/h</p>
-                  <span>Wind Speedddd</span>
+                  <span>Wind Speeddd</span>
                 </div>
               </div>
             </div>
